66222994c887f6985488c9500e783a41ad44c8c4
Satyam Kumar <satyam928532410@gmail.com>
2026-06-13 16:04:36 +0530

    Fixing notification service error

--- src/main/java/com/reminder/service/NotificationService.java
+++ src/main/java/com/reminder/service/NotificationService.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nl.martijndwars.webpush.PushService;
 import nl.martijndwars.webpush.Subscription;
-import nl.martijndwars.webpush.Notification.Builder;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
